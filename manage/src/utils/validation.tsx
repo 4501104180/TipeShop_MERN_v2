686cdcbe92c59dd0d89c7a6962adfdcab8c89abf
Phan Huỳnh Phúc <72651171+4501104180@users.noreply.github.com>
2022-10-13 01:06:33 +0700

    ValidationProducts
    13/10/2022

--- manage/src/utils/validation.tsx
+++ manage/src/utils/validation.tsx
@@ -18,14 +18,46 @@ export const updateAccountValidation = yup.object().shape({
     .matches(/(84|0[3|5|7|8|9])+([0-9]{8})\b/, 'Invalid phone number!'),
   name: yup.string().required('Name is required!'),
 });
-
+// ----------------------Category----------------------
 export const createCategoryValidation = yup.object().shape({
   name: yup.string().required('Name is required!'),
 });
-
+// ----------------------Products----------------------
 export const createProductValidation = yup.object().shape({
   name: yup.string().required('Name is required!'),
+  quantity: yup
+    .string()
+    .required('Quantity is required!')
+    .matches(/^[0-9]+$/, 'Invalid number!'),
+  limit: yup.string().matches(/^[0-9]+$/, 'Invalid number!'),
+  discount: yup.string().matches(/^[0-9]+$/, 'Invalid number!'),
+  discount_rate: yup.string().matches(/^[0-9]{0,2}$/, 'Invalid number! (number 1-99)'),
+  original_price: yup
+    .string()
+    .required('Orginal Price is required!')
+    .matches(/^[0-9]+$/, 'Invalid number!'),
+  price: yup
+    .string()
+    .required('Price is required!')
+    .matches(/^[0-9]+$/, 'Invalid number!'),
+  // images:
 });
 export const updateProductValidation = yup.object().shape({
   name: yup.string().required('Name is required!'),
+  quantity: yup
+    .string()
+    .required('Quantity is required!')
+    .matches(/^[0-9]+$/, 'Invalid number!'),
+  limit: yup.string().matches(/^[0-9]+$/, 'Invalid number!'),
+  discount: yup.string().matches(/^[0-9]+$/, 'Invalid number!'),
+  discount_rate: yup.string().matches(/^[0-9]{0,2}$/, 'Invalid number! (number 1-99)'),
+  original_price: yup
+    .string()
+    .required('Orginal Price is required!')
+    .matches(/^[0-9]+$/, 'Invalid number!'),
+  price: yup
+    .string()
+    .required('Price is required!')
+    .matches(/^[0-9]+$/, 'Invalid number!'),
+  // images:
 });
