7ec4617f6a507b1a697d32f398b9fb6d715ce1c6
Phan Huỳnh Phúc <72651171+4501104180@users.noreply.github.com>
2022-11-06 00:09:13 +0700

    Video youtube ViewProduct - client

--- client/src/components/product/ImageZoom.js
+++ client/src/components/product/ImageZoom.js
@@ -3,6 +3,9 @@ import { useState, Fragment } from 'react';
 import { styled } from '@mui/material/styles';
 import { Stack, Typography } from '@mui/material';
 import Lightbox from 'react-image-lightbox';
+import ReactPlayer from 'react-player/lazy';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import CloseIcon from '@mui/icons-material/Close';
 
 // components
 import ImageLoader from '../ImageLoader';
@@ -15,13 +18,20 @@ const propTypes = {
 	images: array,
 };
 
-const ImageZoom = ({ images }) => {
+const ImageZoom = ({ images, youtube }) => {
+	const a = (youtube) => {
+		if (youtube) {
+			return 3;
+		} else return 4;
+	};
+	const shownCount = a(youtube);
 	const [index, setIndex] = useState(0);
 	const [lightboxIndex, setLightboxIndex] = useState(0);
 	const [isOpenLightbox, setIsOpenLightbox] = useState(false);
-	const shownCount = 4;
-	const imageShow = images.slice(0, shownCount + 1); // Get first shownCount images for showing;
+	const [isOpenLightboxYoutube, setIsOpenLightboxYoutube] = useState(false);
 
+	const imageShow = images.slice(0, shownCount + 1); // Get first shownCount images for showing;
+	// image
 	const handleSwitchImage = (i) => {
 		if (i === shownCount) {
 			setIsOpenLightbox(true);
@@ -33,6 +43,14 @@ const ImageZoom = ({ images }) => {
 		setLightboxIndex(0);
 		setIsOpenLightbox(false);
 	};
+	//youtube
+	const handleSwitchYoutube = () => {
+		setIsOpenLightboxYoutube(true);
+	};
+	const handleCloseLightboxYoutube = () => {
+		setIsOpenLightboxYoutube(false);
+	};
+	console.log(shownCount);
 	return (
 		<Fragment>
 			<RootStyle>
@@ -49,9 +67,33 @@ const ImageZoom = ({ images }) => {
 					}}
 					onClick={() => setIsOpenLightbox(true)}
 				/>
+				{youtube && (
+					<MiniYoutube onClick={() => handleSwitchYoutube()}>
+						<ReactPlayer
+							url={youtube}
+							playing={true}
+							light={true}
+							muted={true}
+							pip={true}
+							width={67}
+							height={67}
+							style={{ padding: '5px 2px 3px 4px', marginTop: '-70px' }}
+						/>
+						<ViewYoutube>
+							<YouTubeIcon style={{ margin: 'auto' }} />
+						</ViewYoutube>
+					</MiniYoutube>
+				)}
 				<Stack direction="row" spacing={1}>
 					{imageShow.map((image, i) => (
-						<MiniImage key={i} className={index === i ? 'active' : ''} onClick={() => handleSwitchImage(i)}>
+						<MiniImage
+							key={i}
+							className={index === i ? 'active' : ''}
+							onClick={() => {
+								handleSwitchImage(i);
+								handleCloseLightboxYoutube();
+							}}
+						>
 							<ImageLoader
 								src={distinguishImage(image)}
 								alt={image}
@@ -78,6 +120,23 @@ const ImageZoom = ({ images }) => {
 					onMoveNextRequest={() => setLightboxIndex((lightboxIndex + 1) % images.length)}
 					imageCaption={`${lightboxIndex + 1}/${images.length}`}
 				/>
+			)}
+			{isOpenLightboxYoutube && (
+				<div style={{ position: 'absolute', left: '200px', top: '200px' }}>
+					<button
+						style={{
+							position: 'absolute',
+							left: '386px',
+							top: '7px',
+							background: 'currentColor',
+							border: 'currentColor',
+						}}
+						onClick={() => handleCloseLightboxYoutube()}
+					>
+						<CloseIcon style={{ color: 'white' }} />
+					</button>
+					<ReactPlayer url={youtube} width={418} controls={true} muted={true} />
+				</div>
 			)}
 		</Fragment>
 	);
@@ -110,7 +169,6 @@ const MiniImage = styled('div')({
 		border: '1px solid #f53d2d',
 	},
 });
-
 const ViewAllText = styled(Typography)({
 	position: 'absolute',
 	width: '100%',
@@ -126,6 +184,29 @@ const ViewAllText = styled(Typography)({
 	'&:hover': {
 		backgroundColor: 'rgba(0, 0, 0, 0.65)',
 	},
+});
+const MiniYoutube = styled('div')({
+	position: 'relative',
+	left: '312px',
+	top: '70px',
+	width: '70px',
+	height: '70px',
+	backgroundSize: '100% auto',
+	borderRadius: '5px',
+	cursor: 'pointer',
+});
+const ViewYoutube = styled(Typography)({
+	position: 'absolute',
+	width: '100%',
+	height: '100%',
+	fontSize: '13px',
+	fontWeight: 'bold',
+	color: 'rgb(255, 255, 255)',
+	top: 0,
+	backgroundColor: 'rgba(0, 0, 0, 0.7)',
+	borderRadius: '5px',
+	display: 'flex',
+	alignItems: 'center',
 });
 
 ImageZoom.propTypes = propTypes;
